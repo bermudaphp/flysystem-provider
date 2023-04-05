3e2d1f56250cbbe28041294be97899eb83007546
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-04-05 22:33:35 +0200

    Update OperatorFactory.php

--- src/OperatorFactory.php
+++ src/OperatorFactory.php
@@ -3,12 +3,25 @@
 namespace Bermuda\Flysystem;
 
 use League\Flysystem\Filesystem;
+use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\PathNormalizer;
+use League\Flysystem\UrlGeneration\PublicUrlGenerator;
+use League\Flysystem\UrlGeneration\TemporaryUrlGenerator;
+use Psr\Container\ContainerInterface;
+use function Bermuda\Config\cget;
+use function Bermuda\Config\conf;
 
 final class OperatorFactory
 {
-    public function __invoke(): Filesystem
+    public function __invoke(ContainerInterface $container): Filesystem
     {
-        return new Filesystem(new LocalFilesystemAdapter(getcwd()));
+        return new Filesystem(
+            $container->get(FilesystemAdapter::class),
+            conf($container)[ConfigProvider::configKey][ConfigProvider::config] ?? [],
+            $container->get(PathNormalizer::class),
+            $container->get(PublicUrlGenerator::class),
+            cget($container, TemporaryUrlGenerator::class)
+        );
     }
 }
