361d4df32e2581f46b970b87c061e51b2784d793
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2023-04-05 22:34:14 +0200

    Update ConfigProvider.php

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -2,17 +2,42 @@
 
 namespace Bermuda\Flysystem;
 
+use League\Flysystem\FilesystemAdapter;
 use League\Flysystem\FilesystemOperator;
+use League\Flysystem\Local\LocalFilesystemAdapter;
+use League\Flysystem\PathNormalizer;
+use League\Flysystem\UrlGeneration\PrefixPublicUrlGenerator;
+use League\Flysystem\UrlGeneration\PublicUrlGenerator;
+use League\Flysystem\UrlGeneration\ShardedPrefixPublicUrlGenerator;
+use League\Flysystem\UrlGeneration\TemporaryUrlGenerator;
+use League\Flysystem\WhitespacePathNormalizer;
+use Psr\Container\ContainerInterface;
+use function Bermuda\Config\conf;
 
 final class ConfigProvider extends \Bermuda\Config\ConfigProvider
 {
+    public const configKey = 'filesystem';
+    public const publicUrl = 'publicUrl';
+    public const config = 'config';
+
     /**
      * @inheritDoc
      */
     protected function getFactories(): array
     {
         return [
-            FilesystemOperator::class => OperatorFactory::class
+            FilesystemOperator::class => OperatorFactory::class,
+            FilesystemAdapter::class => static fn() => new LocalFilesystemAdapter(getcwd()),
+            PathNormalizer::class => static fn() => new WhitespacePathNormalizer(),
+            PublicUrlGenerator::class => static function(ContainerInterface $container) {
+                try {
+                    $publicUrl = conf($container)[self::configKey][self::publicUrl] ?? null ;
+                } catch (\Throwable) { $publicUrl = null; }
+
+                if ($publicUrl === null) return null ;
+                return is_array($publicUrl) ? new ShardedPrefixPublicUrlGenerator($publicUrl)
+                    : new PrefixPublicUrlGenerator($publicUrl);
+            },
         ];
     }
 }
